5fa90db0ede5bdfd04fc33e3cd37ef434d702dd5
Adrian Biedrzycki <a@modules.io>
2017-12-30 16:45:36 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const stream = require('stream');
+const childProcess = require('child_process');
+
+const isWindows = process.platform === 'win32';
+
+const createReadStream = (filePath, {tail = null} = {}) => {
+  const cmd = (() => {
+    if (!isWindows) {
+      return ['tail', '-f'].concat(tail !== null ? ['-n', String(tail)] : []).concat(filePath);
+    } else {
+      return ['powershell.exe', '-Command', `Get-Content -Wait ${tail !== null ? ('-Tail ' + tail) : ''} "${filePath.replace(/"/g, '\\"')}"`];
+    }
+  })();
+
+  const s = new stream.PassThrough();
+  s.destroy = () => {
+    cp.kill();
+
+    cp.stdout.unpipe(s);
+    cp.stderr.unpipe(s);
+  };
+
+  const cp = childProcess.spawn(cmd[0], cmd.slice(1));
+  cp.on('error', err => {
+    s.emit('error', err);
+  });
+  cp.stdout.pipe(s);
+  cp.stderr.pipe(s);
+
+  return s;
+};
+
+module.exports = {
+  createReadStream,
+};
